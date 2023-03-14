09c981558541e5765621e2872b67055452961508
Tr-Junior <8xnecrox8@gmail.com>
2023-03-13 21:59:16 -0300

    Teste para Estágio Target Sistemas

---
+++ calculoPercentual/calculoPercentual.js
@@ -0,0 +1,26 @@
+
+
+function financial(x) {
+    return Number.parseFloat(x).toFixed(2);
+}
+
+var sp = 67.83643,
+    rj = 36.67866,
+    mg = 29.22988,
+    es = 27.16548,
+    outr = 19.84953;
+
+var total = (sp + rj + mg + es + outr);
+
+var percentualSp = (sp / total) * 100;
+var percentualRj = (rj / total) * 100;
+var percentualMg = (mg / total) * 100;
+var percentualEs = (es / total) * 100;
+var percentualOutr = (outr / total) * 100;
+
+
+console.log("Porcentagem do faturamento de SP %f%", financial(percentualSp));
+console.log("Porcentagem do faturamento de RJ %f%", financial(percentualRj));
+console.log("Porcentagem do faturamento de MG %f%", financial(percentualMg));
+console.log("Porcentagem do faturamento de ES %f%", financial(percentualEs));
+console.log("Porcentagem do faturamento de Outros %f%", financial(percentualOutr));
