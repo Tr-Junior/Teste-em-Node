87f47b29821fd0539ad1b66793ce2dd28eb8018a
Tr-Junior <8xnecrox8@gmail.com>
2023-03-14 23:35:33 -0300

    Teste para Estágio Target Sistemas

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,22 +1,22 @@
 
 
-var numero1 = 0, numero2 = 1, numero3 = 0;
+var anterior = 0, proximo = 1, soma = 0;
 
 let numero = [0, 1, 1, 2, 3, 5, 10, 12, 22, 33, 43, 89, 144, 233, 377, 610, 987, 1597, 2584];
 
 
 for (var i = 0; i < numero.length; i++) {
-    if (numero[i] > numero3) {
-        numero3 = numero1 + numero2;
-        numero1 = numero2;
-        numero2 = numero3;
+    if (numero[i] > soma) {
+        soma = anterior + proximo;
+        anterior = proximo;
+        proximo = soma;
 
 
 
         if (numero[i] == 0 | numero[i] == 1) {
             console.log('numero %d esta na sequência', numero[i]);
         }
-        if (numero[i] == numero3) {
+        if (numero[i] == soma) {
             console.log('numero %d esta na sequência ', numero[i]);
         } else {
             console.log('numero %d não esta na sequência', numero[i]);
