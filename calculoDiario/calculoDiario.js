adc6bb817dcb78875a969a4cce85719047718384
Tarcísio Júnior <8xnecrox8@gmail.com>
2023-05-25 10:09:22 -0300

    Update calculoDiario.js

--- calculoDiario/calculoDiario.js
+++ calculoDiario/calculoDiario.js
@@ -1,13 +1,11 @@
 const fs = require('fs');
 
 fs.readFile('./dados.json', 'utf-8', (error, data) => {
-
     const dados = JSON.parse(data);
 
     var maior = 0, menor = 0, soma = 0, media = 0, aux = 0;
 
     for (var i = 0; i < dados.length; i++) {
-
         if (dados[i].valor != 0) {
             aux = dados[i].valor;
 
@@ -22,34 +20,24 @@ fs.readFile('./dados.json', 'utf-8', (error, data) => {
             }
 
             soma += dados[i].valor;
-
         }
-
     }
 
     console.log("Maior faturamento do mês: " + maior);
     console.log("Menor faturamento do mês: " + menor);
-
 
     media = soma / dados.length;
 
     var diasFaturamento = [];
 
     for (var j = 0; j < dados.length; j++) {
-
         if (dados[j].valor != 0) {
             if (dados[j].valor > media) {
-                diasFaturamento += (dados[j].dia) + ', ';
+                diasFaturamento.push(dados[j].dia);
             }
         }
-
     }
+
     console.log("Media mensal: " + media);
-    console.log("Dias em que o faturamento foi maior que a media mensal: " + diasFaturamento);
-
-
+    console.log("Dias em que o faturamento foi maior que a media mensal: " + diasFaturamento.join(', '));
 });
-
-
-
-
